78a9336bf36e54c61238cebec46819af4be18571
email <racbernazgul@gmail.com>
2023-09-04 23:47:42 +0300

    doctors.jsx

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,11 +1,23 @@
 import Container from "react-bootstrap/Container"
+import { doctorData } from "../helper/data"
+import  Row  from "react-bootstrap/Row"
+import  Col  from "react-bootstrap/Col"
 
 const Doctors = () => {
+  console.log(doctorData)
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(8, 162, 168)" }}>
         Our Doctors
       </h3>
+      
+      <Row>
+        {doctorData.map((item) => (
+        <Col></Col>
+      ))}
+      </Row>
+
+      
     </Container>
   )
 }
