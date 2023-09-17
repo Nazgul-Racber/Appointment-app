7323757d2ce151cf5c1f14aeff987642a006ac6f
email <racbernazgul@gmail.com>
2023-09-18 00:27:10 +0300

    consulted: !item.consulted

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -3,29 +3,33 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { TiDelete } from "react-icons/ti";
 
-
 const AppointmentList = ({ apps, setApps }) => {
   console.log(apps);
 
   const handleDelete = (id) => {
-    setApps(apps.filter((item) => item.id !== id))
-  }
+    setApps(apps.filter((item) => item.id !== id));
+  };
 
-  const handleDoubleClick = () => {
-    
-  }
+  const handleDoubleClick = (id) => {
+    setApps(
+      apps.map((item) =>
+        item.id == id ? { ...item, consulted: !item.consulted } : item
+      )
+    );
+  };
+  console.log(apps);
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(180, 24, 204)" }}>
         Appointment List
       </h3>
-      {apps.length < 1 && <img src="./img/appointment.jpg" width="70%"/>}
+      {apps.length < 1 && <img src="./img/appointment.jpg" width="70%" />}
 
       {apps.map(({ id, patient, consulted, doctor, day }) => (
         <div
           key={id}
           className={consulted ? "appointments consulted" : "appointments"}
-          onDoubleClick={handleDoubleClick}
+          onDoubleClick={() => handleDoubleClick(id)}
         >
           <Row className="justify-content-between align-items-center">
             <Col>
