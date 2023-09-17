b60bede93d189812931c3e001679425349b1a7d0
email <racbernazgul@gmail.com>
2023-09-17 17:26:13 +0300

    consulted

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,23 +1,32 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { TiDelete } from "react-icons/ti";
+
 
 const AppointmentList = ({ apps }) => {
   console.log(apps);
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
+      <h3 className="display-6 mb-2" style={{ color: "rgb(180, 24, 204)" }}>
         Appointment List
       </h3>
       {apps.map(({ id, patient, consulted, doctor, day }) => (
-        <div key={id}>
+        <div
+          key={id}
+          className={consulted ? "appointments consulted" : "appointments"}
+        >
           <Row>
             <Col>
               <h4>{patient}</h4>
               <h5>{doctor}</h5>
             </Col>
-            <Col>{day}</Col>
-            <Col>Icon</Col>
+            <Col>
+              <h5>{day}</h5>
+            </Col>
+            <Col>
+              <TiDelete className="text-danger fs-1" />
+            </Col>
           </Row>
         </div>
       ))}
