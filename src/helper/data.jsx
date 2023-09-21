46f28be1c49475e2dc4044ca0d0716d0740fd81b
email <racbernazgul@gmail.com>
2023-09-21 10:56:58 +0300

    add subtrct: data.jsx

--- src/helper/data.jsx
+++ src/helper/data.jsx
@@ -20,13 +20,13 @@ export const appointmentData = [
     consulted: true,
     doctor: "Dr. Jasmine Carpanter",
   },
-  {
-    id: 4,
-    patient: "XYZ Black",
-    day: new Date().toISOString().slice(0, 10),
-    consulted: false,
-    doctor: "Dr. Jasmine Carpanter",
-  },
+  // {
+  //   id: 4,
+  //   patient: "XYZ Black",
+  //   day: new Date().toISOString().slice(0, 10),
+  //   consulted: false,
+  //   doctor: "Dr. Jasmine Carpanter",
+  // },
 ];
 
 export const doctorData = [
@@ -78,4 +78,4 @@ export const doctorData = [
     dep: "Oncology",
     img: "./img/dr8.jpg",
   },
-]
+];
